34975738d0dcc96da7248f71d2f574a6e5b3d828
progali007 <code.progali@gmail.com>
2022-11-10 03:38:17 +0330

    Desinged component

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card({children, className}) {
+  return (
+    <div className={`px-3 py-4 w-full bg-white rounded-md shadow-sm md:p-6 ${className}`}>
+      {children}
+    </div>
+  )
+}
+
+export default Card
