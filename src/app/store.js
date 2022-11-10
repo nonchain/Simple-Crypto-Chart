a6b2c61ab1cec31c0e63cdfa05299743f5e0c3f8
progali007 <code.progali@gmail.com>
2022-11-10 12:40:14 +0330

    Add singleCandleChartSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import multiCandlesChartSlice from '../features/multiCandlesChartSlice';
+import singleCandleChartSlice from '../features/singleCandleChartSlice';
 
 export const store = configureStore({
    reducer: {
       multiCandles: multiCandlesChartSlice,
+      singleCandles: singleCandleChartSlice,
    }
 })
