448f3c2220c83e6d4aa8739edabd187a2c33e5d2
progali007 <code.progali@gmail.com>
2022-11-10 10:16:41 +0330

    Remove console.log

--- src/components/SingleCandle.jsx
+++ src/components/SingleCandle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SingleCandle({ max, value }) {
    const percent = Math.floor(value*100) / max;
-   console.log(value)
+
    return (
       <div className='absolute top-1 left-1/2 h-[83%] w-11 rounded bg-gray-100'>
          <div style={{ height: `${percent}%` }} className='w-full absolute bottom-0 bg-blue-500'></div>
