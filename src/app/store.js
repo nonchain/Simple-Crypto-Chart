b691fdd053735b05ef8920c7d6f7450e0a67c190
progali007 <code.progali@gmail.com>
2022-11-10 13:31:13 +0330

    Delete singleCandlesChartSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import multiCandlesChartSlice from '../features/multiCandlesChartSlice';
-import singleCandleChartSlice from '../features/singleCandleChartSlice';
 
 export const store = configureStore({
    reducer: {
       multiCandles: multiCandlesChartSlice,
-      singleCandles: singleCandleChartSlice,
    }
 })
