c6a750ccf1c45d007e248129acd9da97ddc70950
progali007 <code.progali@gmail.com>
2022-11-10 13:33:13 +0330

    Add Charts & Filter components - Styling the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Card from './components/Card'
+import MultiCandlesChart from './components/MultiCandlesChart'
+import CandlesFilter from './components/CandlesFilter'
+import SingleCandlesChart from './components/SingleCandlesChart'
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      BTC-Chart
+    <div className="bg-gray-50 p-4 min-h-screen flex flex-col gap-3 md:grid md:grid-cols-3 md:grid-rows-2">
+      <Card className='md:col-span-2'>
+        <MultiCandlesChart />
+      </Card>
+      <Card className='order-3 md:col-span-1'>
+        <SingleCandlesChart />
+      </Card>
+      <Card className='order-2 md:col-span-3 md:row-start-2'>
+        <CandlesFilter />
+      </Card>
     </div>
   )
 }
