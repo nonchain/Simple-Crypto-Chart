605d4abde153441c6997f74adbc576045e76f288
progali007 <code.progali@gmail.com>
2022-11-10 12:26:47 +0330

    Change label font-weight

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -20,7 +20,7 @@ function Checkbox({ name, id, isShow, ...rest}) {
    return (
       <div className='flex items-center gap-2 '>
          <input type='checkbox' name={name} id={id} checked={checked} onChange={onCheckedHandler} {...rest} className={`cursor-pointer appearance-none w-5 h-5 ${checked ? bgColor[id] : 'bg-gray-100'} border-2 border-gray-500 rounded-md`} />
-         <label htmlFor={id} className='cursor-pointer'>{name}</label>
+         <label htmlFor={id} className='cursor-pointer font-medium'>{name}</label>
       </div>
    )
 }
