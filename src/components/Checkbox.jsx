daf1b9f0faf0ef3af5f5c17590793fb5b241c84c
progali007 <code.progali@gmail.com>
2022-11-10 12:21:43 +0330

    Add functionality for change candles visibility state

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { showCandlesHandler  } from '../features/multiCandlesChartSlice';
 
 const bgColor = {
    high: 'bg-[#06cc8d]',
@@ -6,11 +8,15 @@ const bgColor = {
    low: 'bg-[#fa3628]',
 }
 
-function Checkbox({ name, id, ...rest}) {
-   const [checked, setChecked] = useState(false);
+function Checkbox({ name, id, isShow, ...rest}) {
+   const [checked, setChecked] = useState(isShow);
+   const dispatch = useDispatch();
+
    const onCheckedHandler = () => {
       setChecked(!checked);
+      dispatch(showCandlesHandler({id, status: !checked}))
    }
+
    return (
       <div className='flex items-center gap-2 '>
          <input type='checkbox' name={name} id={id} checked={checked} onChange={onCheckedHandler} {...rest} className={`cursor-pointer appearance-none w-5 h-5 ${checked ? bgColor[id] : 'bg-gray-100'} border-2 border-gray-500 rounded-md`} />
